896ec7bcecda03434efbf8e999d23301a29deef8
Hendrik Mans <hendrik@mans.de>
2022-07-26 14:02:21 +0200

    Eh

--- apps/examples/src/game/Game.tsx
+++ apps/examples/src/game/Game.tsx
@@ -1,3 +1,24 @@
+type Action<Names extends string> = () => Names | null
+
+type State<Names extends string> = Record<string, Action<Names>>
+
+type StateMachine<Names extends string> = {
+	[name in Names]: State<Names>
+}
+
+const StateMachine = <S extends StateMachine<Names>, Names extends string>(
+	config: S
+) => {}
+
+const config: StateMachine<"start" | "game"> = {
+	start: {
+		enterGame: () => "game"
+	},
+	game: {}
+}
+
+StateMachine(config)
+
 export const Game = () => {
 	return <p>I'm the game</p>
 }
